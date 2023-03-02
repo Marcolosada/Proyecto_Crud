a30aa6ed709bf3dd6d8018f328e54fbe73739c29
Marcolosada <marco937234@gmail.com>
2023-03-02 13:04:06 -0600

    conexion al servidor sin estilos

---
+++ Proyecto_Crud-main/proyecto_crud/src/components/Formulario.vue
@@ -0,0 +1,219 @@
+<script>
+import axios from "axios";
+let API_URL = `/api/guardarPersonasUxd.php`;
+export default {
+    data() {
+        return {
+            nombre: "",
+            edad: "",
+            estadoCivil: "",
+            trabajo: "",
+            residencia: "",
+            cita: "",
+            citaAutor: "",
+            bio: "",
+            personalidad01: "",
+            personalidad02: "",
+            personalidad03: "",
+            personalidad04: "",
+            objetivos: "",
+            frustraciones: "",
+            motivaciones: "",
+            porcentaje: "",
+            marcas: "",
+        };
+    },
+    methods: {
+        summit() {
+            API_URL + this.pagina;
+            console.log(API_URL);
+            axios.post("/api/guardarPersonasUxd.php", {
+                nombre: this.nombre,
+                edad: this.edad,
+                estadoCivil: this.estadoCivil,
+                trabajo: this.trabajo,
+                residencia: this.residencia,
+                cita: this.cita,
+                citaAutor: this.citaAutor,
+                bio: this.bio,
+                personalidad01: this.personalidad01,
+                personalidad02: this.personalidad02,
+                personalidad03: this.personalidad03,
+                personalidad04: this.personalidad04,
+                objetivos: this.objetivos,
+                frustraciones: this.frustraciones,
+                motivaciones: this.motivaciones,
+                porcentaje: this.porcentaje,
+                marcas: this.marcas,
+                //completar las variables, estas deben llamarse como las que se recibirán en el backend sin el símbolo del dolar $
+            })
+                .then((response) => {
+                    console.log(response.status)
+                });
+        },
+    },
+};
+
+</script>
+<template>
+    <form class="w-full max-w-lg">
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="nombre">
+        NOMBRE
+      </label>
+      <input v-model="nombre" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-grey-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="nombre" type="text" placeholder="Jane">
+    </div>
+    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="edad">
+        EDAD
+      </label>
+      <input  v-model="edad" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="edad" type="string" placeholder="22">
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+
+    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="estadoCivil">
+        ESTADO CIVIL
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="estadoCivil">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="trabajo">
+        TRABAJO
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="trabajo" type="text" placeholder=" ">
+    </div>
+
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="residencia">
+        RESIDENCIA
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="residencia" type="string" placeholder="  ">
+    </div>
+
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="cita">
+        CITA
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="cita" type="text" placeholder="  ">
+    </div>
+
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="citaAutor">
+        CITA AUTOR
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="citaAutor" type="text" placeholder="  ">
+    </div>
+
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="bio">
+        BIO
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="bio" type="string" placeholder="22">
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="personalidad01">
+        PERSONALIDAD 1
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="personalidad01" type="text" placeholder="  ">
+    </div>
+
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="personalidad02">
+        PERSONALIDAD 2
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="personalidad02" type="string" placeholder="22">
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="personalidad03">
+        PERSONALIDAD 3
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="personalidad03" type="text" placeholder="  ">
+    </div>
+
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="personalidad04">
+        PERSONALIDAD 4
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="personalidad04" type="string" placeholder="22">
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="objetivos">
+        OBJETIVOS
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="objetivos" type="text" placeholder="  ">
+    </div>
+
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="frustraciones">
+        FRUSTRACIONES
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="frustraciones" type="string" placeholder="22">
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="motivaciones">
+        MOTIVACIONES
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="motivaciones" type="text" placeholder="  ">
+    </div>
+
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="porcentaje">
+        PORCENTAJES
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="porcentaje" type="string" placeholder="22">
+    </div>
+  </div>
+
+  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="marcas">
+        MARCAS
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="marcas" type="string" placeholder="22">
+    </div>
+
+    <button
+    @click="summit()"
+    
+     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        ENVIAR
+        
+    </button>
+
+</form>
+</template>
