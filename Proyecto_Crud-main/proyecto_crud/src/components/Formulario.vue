38169d78c7bb2a5cc4af81a21225234f2ebbd84d
Marcolosada <marco937234@gmail.com>
2023-03-08 15:09:09 -0600

    formulario ya validado

--- Proyecto_Crud-main/proyecto_crud/src/components/Formulario.vue
+++ Proyecto_Crud-main/proyecto_crud/src/components/Formulario.vue
@@ -1,59 +1,5 @@
-<script>
-import axios from "axios";
-let API_URL = `/api/guardarPersonasUxd.php`;
-export default {
-    data() {
-        return {
-            nombre: "",
-            edad: "",
-            estadoCivil: "",
-            trabajo: "",
-            residencia: "",
-            cita: "",
-            citaAutor: "",
-            bio: "",
-            personalidad01: "",
-            personalidad02: "",
-            personalidad03: "",
-            personalidad04: "",
-            objetivos: "",
-            frustraciones: "",
-            motivaciones: "",
-            porcentaje: "",
-            marcas: "",
-        };
-    },
-    methods: {
-        summit() {
-            API_URL + this.pagina;
-            console.log(API_URL);
-            axios.post("/api/guardarPersonasUxd.php", {
-                nombre: this.nombre,
-                edad: this.edad,
-                estadoCivil: this.estadoCivil,
-                trabajo: this.trabajo,
-                residencia: this.residencia,
-                cita: this.cita,
-                citaAutor: this.citaAutor,
-                bio: this.bio,
-                personalidad01: this.personalidad01,
-                personalidad02: this.personalidad02,
-                personalidad03: this.personalidad03,
-                personalidad04: this.personalidad04,
-                objetivos: this.objetivos,
-                frustraciones: this.frustraciones,
-                motivaciones: this.motivaciones,
-                porcentaje: this.porcentaje,
-                marcas: this.marcas,
-                //completar las variables, estas deben llamarse como las que se recibirán en el backend sin el símbolo del dolar $
-            })
-                .then((response) => {
-                    console.log(response.status)
-                });
-        },
-    },
-};
 
+<script>
 </script>
 <template>
     <form class="w-full max-w-lg">
@@ -62,7 +8,7 @@ export default {
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="nombre">
         NOMBRE
       </label>
-      <input v-model="nombre" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-grey-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="nombre" type="text" placeholder="Jane">
+      <input v-model="nombre" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-grey-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="nombre" type="text" placeholder="Nombre">
     </div>
     <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="edad">
@@ -206,12 +152,8 @@ export default {
       <input v-model="marcas" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="marcas" type="string" placeholder="22">
     </div>
 
-    <button
-    @click="summit()"
-    
-     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        ENVIAR
-        
+    <button @click="Enviar()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button">
+        Enviar
     </button>
 
 </form>
